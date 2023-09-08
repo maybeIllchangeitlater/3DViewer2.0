a616bc6fedb33a8c5d3492d7fef644a4e4c783c0
Susanne Luigi <susannel@at-h6.msk.21-school.ru>
2023-09-08 22:16:19 +0300

    s

--- build-view-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/gpt_help.dir/gpt_help_autogen/EWIEGA46WW/qrc_style.cpp.o.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/gpt_help.dir/gpt_help_autogen/EWIEGA46WW/qrc_style.cpp.o: \
-  /Users/susannel/3dviewer2/build-view-Desktop_x86_darwin_generic_mach_o_64bit-Debug/gpt_help_autogen/EWIEGA46WW/qrc_style.cpp
--- build-view-Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/gpt_help_autogen.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for gpt_help_autogen.
-# This may be replaced when dependencies are built.
